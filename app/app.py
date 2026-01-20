9386ba0025c3669d38d1e720987dde772b815520
Joel Lopez <jlopez@HOSPITALALCIVAR.GYE>
2026-01-20 12:31:28 -0500

    Proyecto institucional - estructura inicial

---
+++ app/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)   # 👈 AQUÍ se define la app
+
+
+@app.route("/")
+def inicio():
+    return render_template("manual/inicio.html")
+
+@app.route("/vision")
+def vision():
+    return render_template("manual/vision.html")
+
+@app.route("/mision")
+def mision():
+    return render_template("manual/mision.html")
+
+@app.route("/valores")
+def valores():
+    return render_template("manual/valores.html")
+
+@app.route("/manual")
+def manual():
+    return render_template("manual/manual.html")
+
+@app.route("/puntos_doctrinales")
+def puntos_doctrinales():
+    return render_template("manual/puntos_doctrinales.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
