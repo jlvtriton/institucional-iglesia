92f57324cc966c4db8b50c0cd2b78c80921158f6
Joel Lopez <jlopez@HOSPITALALCIVAR.GYE>
2026-01-21 19:09:34 -0500

    Ya estan todos los textos

--- app/textos_biblicos.py
+++ app/textos_biblicos.py
@@ -26,5 +26,179 @@ BIBLIA_RVR = {
     "Galatas_4_19": {
         "texto": "Hijitos míos, por quienes vuelvo a sufrir dolores de parto, hasta que Cristo sea formado en vosotros,",
         "version": "Reina-Valera 1960"
+    },
+    "2_Timoteo_3_16_17": {
+        "texto": "Toda la Escritura es inspirada por Dios, y útil para enseñar, para redarg:uir, para corregir, para instruir en justicia, a fin de que el hombre de Dios sea perfecto, enteramente preparado para toda buena obra.  ",
+        "version": "Reina-Valera 1960"
+    },
+    "2_Pedro_1_20_21": {
+        "texto": "entendiendo primero esto, que ninguna profecía de la Escritura es de interpretación privada, porque nunca la profecía fue traída por voluntad humana, sino que los santos hombres de Dios hablaron siendo inspirados por el Espíritu Santo.",
+        "version": "Reina-Valera 1960"
+    },
+    "Salmos_119_105": {
+        "texto": "Lámpara es a mis pies tu palabra, Y lumbrera a mi camino. ",
+        "version": "Reina-Valera 1960"
+    },
+    "Isaías_40_8": {
+        "texto": "Sécase la hierba, marchítase la flor; mas la palabra del Dios nuestro permanece para siempre.,",
+        "version": "Reina-Valera 1960"
+    },
+    "Deuteronomio_6_4": {
+        "texto": "Oye, Israel: Jehová nuestro Dios, Jehová uno es",
+        "version": "Reina-Valera 1960"
+    },
+    "Mateo_28_19": {
+        "texto": "Por tanto, id, y haced discípulos a todas las naciones, bautizándolos en el nombre del Padre, y del Hijo, y del Espíritu Santo",
+        "version": "Reina-Valera 1960"
+    },
+    "2_Corintios_13_14": {
+        "texto": "La gracia del Señor Jesucristo, el amor de Dios, y la comunión del Espíritu Santo sean con todos vosotros. Amén.",
+        "version": "Reina-Valera 1960"
+    },
+    "Juan_1_1_14": {
+        "texto": "En el principio era el Verbo, y el Verbo era con Dios, y el Verbo era Dios. (14) Y aquel Verbo fue hecho carne, y habitó entre nosotros (y vimos su gloria, gloria como del unigénito del Padre), lleno de gracia y de verdad.",
+        "version": "Reina-Valera 1960"
+    },
+    "Juan_14_6": {
+        "texto": "Jesús le dijo: Yo soy el camino, y la verdad, y la vida; nadie viene al Padre, sino por mí.",
+        "version": "Reina-Valera 1960"
+    },
+    "Hechos_4_12": {
+        "texto": " Y en ningún otro hay salvación; porque no hay otro nombre bajo el cielo, dado a los hombres, en que podamos ser salvos.",
+        "version": "Reina-Valera 1960"
+    },
+    "1_Timoteo_2_5": {
+        "texto": "orque hay un solo Dios, y un solo mediador entre Dios y los hombres, Jesucristo hombre",
+        "version": "Reina-Valera 1960"
+    },
+    "Efesios_2_8_9": {
+        "texto": "Porque por gracia sois salvos por medio de la fe; y esto no de vosotros, pues es don de Dios; no por obras, para que nadie se gloríe.",
+        "version": "Reina-Valera 1960"
+    },
+    "Romanos_3_23_24": {
+        "texto": "por cuanto todos pecaron, y están destituidos de la gloria de Dios, siendo justificados gratuitamente por su gracia, mediante la redención que es en Cristo Jesús",
+        "version": "Reina-Valera 1960"
+    },
+    "Tito_3_5": {
+        "texto": "  nos salvó, no por obras de justicia que nosotros hubiéramos hecho, sino por su misericordia, por el lavamiento de la regeneración y por la renovación en el Espíritu Santo, ",
+        "version": "Reina-Valera 1960"
+    },
+    "Juan_16_8": {
+        "texto": "Y cuando él venga, convencerá al mundo de pecado, de justicia y de juicio.",
+        "version": "Reina-Valera 1960"
+    },
+    "Juan_16_13": {
+        "texto": "Pero cuando venga el Espíritu de verdad, él os guiará a toda la verdad; porque no hablará por su propia cuenta, sino que hablará todo lo que oyere, y os hará saber las cosas que habrán de venir",
+        "version": "Reina-Valera 1960"
+    },
+    "1_Corintios_12_4_7": {
+        "texto": "Ahora bien, hay diversidad de dones, pero el Espíritu es el mismo. Y hay diversidad de ministerios, pero el Señor es el mismo.Y hay diversidad de operaciones, pero Dios, que hace todas las cosas en todos, es el mismo. Pero a cada uno le es dada la manifestación del Espíritu para provecho. ",
+        "version": "Reina-Valera 1960"
+    },
+    "Gálatas_5_22_23": {
+        "texto": "Mas el fruto del Espíritu es amor, gozo, paz, paciencia, benignidad, bondad, fe,mansedumbre, templanza; contra tales cosas no hay ley. ",
+        "version": "Reina-Valera 1960"
+    },
+    "Efesios_4_12": {
+        "texto": "a fin de perfeccionar a los santos para la obra del ministerio, para la edificación del cuerpo de Cristo",
+        "version": "Reina-Valera 1960"
+    },
+    "Mateo_28_19_20": {
+        "texto": "Por tanto, id, y haced discípulos a todas las naciones, bautizándolos en el nombre del Padre, y del Hijo, y del Espíritu Santo; nseñándoles que guarden todas las cosas que os he mandado; y he aquí yo estoy con vosotros todos los días, hasta el fin del mundo. Amén.",
+        "version": "Reina-Valera 1960"
+    },
+    "Hechos_1_11": {
+        "texto": " los cuales también les dijeron: Varones galileos, ¿por qué estáis mirando al cielo? Este mismo Jesús, que ha sido tomado de vosotros al cielo, así vendrá como le habéis visto ir al cielo ",
+        "version": "Reina-Valera 1960"
+    },
+    "1_Tesalonicenses_4_16_17": {
+        "texto": "Porque el Señor mismo con voz de mando, con voz de arcángel, y con trompeta de Dios, descenderá del cielo; y los muertos en Cristo resucitarán primero.  Luego nosotros los que vivimos, los que hayamos quedado, seremos arrebatados juntamente con ellos en las nubes para recibir al Señor en el aire, y así estaremos siempre con el Señor.",
+        "version": "Reina-Valera 1960"
+    },
+    "Apocalipsis_22_12": {
+        "texto": " He aquí yo vengo pronto, y mi galardón conmigo,(G) para recompensar a cada uno según sea su obra.",
+        "version": "Reina-Valera 1960"
+    },
+    "Colosenses_1_28": {
+        "texto": "a quien anunciamos, amonestando a todo hombre, y enseñando a todo hombre en toda sabiduría, a fin de presentar perfecto en Cristo Jesús a todo hombre; ",
+        "version": "Reina-Valera 1960"
+    },
+    "2_Timoteo_3_16": {
+        "texto": "Toda la Escritura es inspirada por Dios, y útil para enseñar, para redarg:uir, para corregir, para instruir en justicia, ",
+        "version": "Reina-Valera 1960"
+    },
+    "1_Tesalonicenses_5_17": {
+        "texto": "Orad sin cesar",
+        "version": "Reina-Valera 1960"
+    },
+    "Marcos_10_45": {
+        "texto": "Porque el Hijo del Hombre no vino para ser servido, sino para servir, y para dar su vida en rescate por muchos.",
+        "version": "Reina-Valera 1960"
+    },
+    "Efesios_4_13": {
+        "texto": " hasta que todos lleguemos a la unidad de la fe y del conocimiento del Hijo de Dios, a un varón perfecto, a la medida de la estatura de la plenitud de Cristo",
+        "version": "Reina-Valera 1960"
+    },
+    "1_Pedro_1_15_16": {
+        "texto": "sino, como aquel que os llamó es santo, sed también vosotros santos en toda vuestra manera de vivir; porque escrito está: Sed santos, porque yo soy santo.",
+        "version": "Reina-Valera 1960"
+    },
+    "Romanos_10_14_15": {
+        "texto": "¿Cómo, pues, invocarán a aquel en el cual no han creído? ¿Y cómo creerán en aquel de quien no han oído? ¿Y cómo oirán sin haber quien les predique? ¿Y cómo predicarán si no fueren enviados? Como está escrito: !!Cuán hermosos son los pies de los que anuncian la paz, de los que anuncian buenas nuevas!",
+        "version": "Reina-Valera 1960"
+    },
+    "Galatas_4_19": {
+        "texto": "Hijitos míos, por quienes vuelvo a sufrir dolores de parto, hasta que Cristo sea formado en vosotros,",
+        "version": "Reina-Valera 1960"
+    },
+    "Galatas_4_19": {
+        "texto": "Hijitos míos, por quienes vuelvo a sufrir dolores de parto, hasta que Cristo sea formado en vosotros,",
+        "version": "Reina-Valera 1960"
+    },
+    "Galatas_4_19": {
+        "texto": "Hijitos míos, por quienes vuelvo a sufrir dolores de parto, hasta que Cristo sea formado en vosotros,",
+        "version": "Reina-Valera 1960"
+    },
+    "Galatas_4_19": {
+        "texto": "Hijitos míos, por quienes vuelvo a sufrir dolores de parto, hasta que Cristo sea formado en vosotros,",
+        "version": "Reina-Valera 1960"
+    },
+    "Galatas_4_19": {
+        "texto": "Hijitos míos, por quienes vuelvo a sufrir dolores de parto, hasta que Cristo sea formado en vosotros,",
+        "version": "Reina-Valera 1960"
+    },
+    "Galatas_4_19": {
+        "texto": "Hijitos míos, por quienes vuelvo a sufrir dolores de parto, hasta que Cristo sea formado en vosotros,",
+        "version": "Reina-Valera 1960"
+    },
+    "Galatas_4_19": {
+        "texto": "Hijitos míos, por quienes vuelvo a sufrir dolores de parto, hasta que Cristo sea formado en vosotros,",
+        "version": "Reina-Valera 1960"
+    },
+    "Galatas_4_19": {
+        "texto": "Hijitos míos, por quienes vuelvo a sufrir dolores de parto, hasta que Cristo sea formado en vosotros,",
+        "version": "Reina-Valera 1960"
+    },
+    "Galatas_4_19": {
+        "texto": "Hijitos míos, por quienes vuelvo a sufrir dolores de parto, hasta que Cristo sea formado en vosotros,",
+        "version": "Reina-Valera 1960"
+    },
+    "Galatas_4_19": {
+        "texto": "Hijitos míos, por quienes vuelvo a sufrir dolores de parto, hasta que Cristo sea formado en vosotros,",
+        "version": "Reina-Valera 1960"
+    },
+    "Galatas_4_19": {
+        "texto": "Hijitos míos, por quienes vuelvo a sufrir dolores de parto, hasta que Cristo sea formado en vosotros,",
+        "version": "Reina-Valera 1960"
+    },
+    "Galatas_4_19": {
+        "texto": "Hijitos míos, por quienes vuelvo a sufrir dolores de parto, hasta que Cristo sea formado en vosotros,",
+        "version": "Reina-Valera 1960"
+    },
+    "Galatas_4_19": {
+        "texto": "Hijitos míos, por quienes vuelvo a sufrir dolores de parto, hasta que Cristo sea formado en vosotros,",
+        "version": "Reina-Valera 1960"
     }
+
+
 }
