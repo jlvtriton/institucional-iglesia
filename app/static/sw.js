a5d6948f4c4c3594bf5c6c9f5751b2ca9e6690e6
Joel Lopez <jlopez@HOSPITALALCIVAR.GYE>
2026-01-21 22:33:57 -0500

    app

---
+++ app/static/sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "ldd-pwa-v1";
+
+const urlsToCache = [
+  "/",
+  "/vision-mision",
+  "/static/css/app.css"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
